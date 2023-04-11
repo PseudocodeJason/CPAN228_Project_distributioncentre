889573c44f5505d15791c3b1d7ee6a9ba69e14b5
Jason <jason.tieu@Hotmail.com>
2023-04-11 01:57:56 -0400

    Added Command Line Runner to add Items and Distribution Centres

--- src/main/java/com/cpan252/distributioncentre/DistributioncentreApplication.java
+++ src/main/java/com/cpan252/distributioncentre/DistributioncentreApplication.java
@@ -1,13 +1,47 @@
 package com.cpan252.distributioncentre;
 
+import java.math.BigDecimal;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.cpan252.distributioncentre.model.DistributionCentre;
+import com.cpan252.distributioncentre.model.Item;
+import com.cpan252.distributioncentre.model.Item.Brand;
+import com.cpan252.distributioncentre.repository.DistributionCentreRepository;
+import com.cpan252.distributioncentre.repository.ItemRepository;
 
 @SpringBootApplication
 public class DistributioncentreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DistributioncentreApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner dataLoader(DistributionCentreRepository repository, ItemRepository repository2){
+		return args ->{
+			repository.save(DistributionCentre.builder()
+			.name("Yorkdale")
+			.item("Gown")
+			.available(50)
+			.longitude(235)
+			.latitude(110).build());
+			repository.save(DistributionCentre.builder()
+			.name("Yorkdale")
+			.item("Shirt")
+			.available(5)
+			.longitude(235)
+			.latitude(110).build());
+			repository2.save(Item.builder()
+			.name("Gown")
+			.brand(Brand.BALENCIAGA)
+			.yearofcreation(2022)
+			.price(new BigDecimal("2000"))
+			.quantity(50).build());
+		};
 	}
 
 }
