e4f7bb0040a4e3ab26e508a7db86d00b5703a77e
Jason <jason.tieu@Hotmail.com>
2023-04-16 18:20:06 -0400

    Added Post and Get Mapping for DistributionCentre ID

--- src/main/java/com/cpan252/distributioncentre/controller/DistributionCentreController.java
+++ src/main/java/com/cpan252/distributioncentre/controller/DistributionCentreController.java
@@ -1,26 +1,50 @@
 package com.cpan252.distributioncentre.controller;
 
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cpan252.distributioncentre.model.DistributionCentre;
+import com.cpan252.distributioncentre.model.Item;
 import com.cpan252.distributioncentre.repository.DistributionCentreRepository;
+import com.cpan252.distributioncentre.repository.ItemRepository;
 
 @RestController
-@RequestMapping(path = "/api/DistributionCentres", produces = "application/json")
+@RequestMapping(path = "/api/DistributionCentre", produces = "application/json")
 public class DistributionCentreController {
     
     private final DistributionCentreRepository distributionCentreRepository;
+    private final ItemRepository itemRepository;
 
-    public DistributionCentreController(DistributionCentreRepository distributionCentreRepository){
+    public DistributionCentreController(DistributionCentreRepository distributionCentreRepository, ItemRepository itemRepository){
         this.distributionCentreRepository = distributionCentreRepository;
+        this.itemRepository = itemRepository;
     }
 
     @GetMapping
-    public Iterable<DistributionCentre> allCentres(){
+    public List<DistributionCentre> allCentres(){
         
-        return distributionCentreRepository.findAll();
+        return (List<DistributionCentre>) distributionCentreRepository.findAll();
+    }
+
+    @GetMapping("/{id}/items")
+    public List<Item> getItemsForCentre(@PathVariable int id) {
+        var currentDistributionCentre = distributionCentreRepository.findById(id);
+        var items = currentDistributionCentre.get().getItem();
+        return items;
+    }
+
+    @PostMapping("/{id}/items")
+    public Item addItemToCentre(@PathVariable int id, @RequestBody Item item) {
+        var currentDistributionCentre = distributionCentreRepository.findById(id);
+        item.setDistributionCentre(currentDistributionCentre.get());
+        var savedItem = itemRepository.save(item);
+        return savedItem;
     }
 }
