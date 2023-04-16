f077de46445cd93cfaeb259cf547832dee11e652
Jason <jason.tieu@Hotmail.com>
2023-04-16 18:59:21 -0400

    Added Delete By Id

--- src/main/java/com/cpan252/distributioncentre/controller/DistributionCentreController.java
+++ src/main/java/com/cpan252/distributioncentre/controller/DistributionCentreController.java
@@ -3,17 +3,22 @@ package com.cpan252.distributioncentre.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cpan252.distributioncentre.model.DistributionCentre;
 import com.cpan252.distributioncentre.model.Item;
+import com.cpan252.distributioncentre.model.dto.CreateCloth;
 import com.cpan252.distributioncentre.repository.DistributionCentreRepository;
 import com.cpan252.distributioncentre.repository.ItemRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(path = "/api/DistributionCentre", produces = "application/json")
@@ -46,5 +51,10 @@ public class DistributionCentreController {
         item.setDistributionCentre(currentDistributionCentre.get());
         var savedItem = itemRepository.save(item);
         return savedItem;
+    }
+
+    @DeleteMapping("/items/{id}")
+    public void deleteCloth(@PathVariable("id") int id) {
+        itemRepository.deleteById(id);
     }
 }
